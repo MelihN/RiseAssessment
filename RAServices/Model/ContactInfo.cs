a985bcbc30d973fe9f5c829a5d58e643a4d3bf6d
Melihn <melihn@gmail.com>
2022-12-06 23:10:18 +0300

    entity, DAL katmanı ve REST servis hazırlandı

--- RAServices/Model/ContactInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RAServices.Model
-{
-    public class ContactInfo
-    {
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string location { get; set; }        
-    }
-}
---
+++ RaModels/ResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaModels
+{
+    public class ResponseModel<T>
+    {
+        public bool ServiceState { get; set; }
+        public bool RequestState { get; set; }
+        public string ErrorMsg { get; set; }
+        public List<T> ItemList { get; set; }
+        public T Item { get; set; }
+        public int TotalRowCount { get; set; }
+    }
+}
