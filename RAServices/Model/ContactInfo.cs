e89f54ff0024c47eecc03052e9e8a68c35b650d4
melih <melih@MELIHN-50C>
2022-12-06 16:03:56 +0300

    Add project files.

---
+++ RAServices/Model/ContactInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace RAServices.Model
+{
+    public class ContactInfo
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string location { get; set; }        
+    }
+}
---
+++ RAServices/Model/Persons.cs
@@ -0,0 +1,12 @@
+﻿namespace RAServices.Model
+{
+    public class Persons
+    {
+        //Mongo DB UUID formatında tutmaktadır
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+        public List<ContactInfo> ContactInfos { get; set; }
+    }
+}
