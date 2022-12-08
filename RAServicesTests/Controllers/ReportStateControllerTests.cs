dcdfacb20114a1c94e267027d191293f255860e5
Melihn <melihn@gmail.com>
2022-12-08 03:38:33 +0300

    Servis Test projesi eklendi

---
+++ RAServicesTests/Controllers/ReportStateControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RAServices.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAServices.Controllers.Tests
+{
+    [TestClass()]
+    public class ReportStateControllerTests
+    {
+        [TestMethod()]
+        public void GetOneTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
